2e3e98cfbe6f5112d749186a219db9c8cf83dc26
Davey Schulz <davey.schulz@hotmail.com>
2023-11-10 19:36:34 +0100

    setup project

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+const Home = () => {
+  return (
+    <div className="text-6xl text-green-500">
+      <Button>Click me!</Button>
+    </div>
+  );
+};
+
+export default Home;
