760f5b039501f3e48a1f06647fcb1c3e4ff87031
Davey Schulz <davey.schulz@hotmail.com>
2023-11-10 20:05:24 +0100

    Setup clerke Auth

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,9 +1,10 @@
-import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
 const DashboardPage = () => {
   return (
     <div>
       <p>Dashboard</p>
+      <UserButton afterSignOutUrl="/" />
     </div>
   );
 };
