3b35df2f7cc0d8a530158a2dd4de9526e2cbc239
Davey Schulz <davey.schulz@hotmail.com>
2023-11-10 19:45:08 +0100

    Refactored folder structure

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+const DashboardPage = () => {
+  return (
+    <div>
+      <p>Dashboard</p>
+    </div>
+  );
+};
+
+export default DashboardPage;
