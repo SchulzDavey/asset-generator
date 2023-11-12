e03bb9cc6238b711a4cb9781752c3b4a676e16ef
Davey Schulz <davey.schulz@hotmail.com>
2023-11-12 19:06:19 +0100

    Added the free trial progress bar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -13,6 +13,7 @@ import {
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import FreeCounter from "./FreeCounter";
 
 const routes = [
   {
@@ -58,11 +59,15 @@ const routes = [
   },
 ];
 
-const Sidebar = () => {
+interface SidebarProps {
+  apiLimitCount: number;
+}
+
+const Sidebar = ({ apiLimitCount = 0 }: SidebarProps) => {
   const pathname = usePathname();
 
   return (
-    <div className="space-y-4 py-4 fle flex-col h-full bg-[#111827] text-white">
+    <div className="space-y-4 py-4 flex flex-col h-full bg-[#111827] text-white">
       <div className="px-3 py-2 flex-1">
         <Link href="/dashboard" className="flex items-center pl-3 mb-14">
           <div className="relative w-8 h-8 mr-4">
@@ -90,6 +95,7 @@ const Sidebar = () => {
           ))}
         </div>
       </div>
+      <FreeCounter apiLimitCount={apiLimitCount} />
     </div>
   );
 };
